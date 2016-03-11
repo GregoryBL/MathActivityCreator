b3846e53cc744d97237fc13a8d64cf2d9dbe472f
Greg Berns-Leone <gberns.l@gmail.com>
2016-03-10 21:40:12 -0600

    Initial Commit

---
+++ MathActivitityCreator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MathActivitityCreator
+//
+//  Created by Greg on 3/10/16.
+//  Copyright © 2016 GregApps. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
